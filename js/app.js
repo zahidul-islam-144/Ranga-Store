027506e611fe28a36138803868f3c5961f0281e5
Zahidul Islam <zahidul.islam1@northsouth.edu>
2021-09-13 23:49:16 +0600

    comment edited

--- js/app.js
+++ js/app.js
@@ -19,7 +19,8 @@ const showProducts = (products) => {
         <img class="product-image" src=${product.image}></img>    // bug fixed 
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
-        <h5>Rating: ${product.rating.rate}  Totatal rated: ${product.rating.count}</h5> // rating and total rating 
+        // rating and total rating 
+        <h5>Rating: ${product.rating.rate}  Totatal rated: ${product.rating.count}</h5> 
         <h2>Price: $ ${product.price}</h2>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button  id="details-btn" class="btn btn-danger">Details</button>
